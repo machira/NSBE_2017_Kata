75e134a29c586189ca24136f0e23bcdef7921c21
Raymond Machira <raymond.machira+git@gmail.com>
2017-03-20 01:55:50 -0400

    Update python up down to use angle brackets.

---
+++ java/FizzBuzz/README.md
@@ -0,0 +1,11 @@
+FizzBuzzJava
+============
+
+What is Fizz Buzz?
+
+Write a program that prints the numbers 1 to 100. 
+For multiples of 3, print "Fizz" instead of the number and for multiples of 5, print
+"Buzz". For numbers that are multiples of both 3 and 5 print "FizzBuzz."
+
+This is a Java implementation of Fizz Buzz.
+
--- python/updown/updown_tests.py
+++ python/updown/updown_tests.py
@@ -3,30 +3,30 @@ import updown
 
 
 class updown_tests(unittest.TestCase):
-    def test_one_paren(self):
-        self.assertEqual(1, updown.count("("))
+    def test_one_bracket(self):
+        self.assertEqual(1, updown.count(">"))
 
     def test_invalid_input(self):
-        self.assertRaises(ValueError, updown.count, "not_a_parenthesis")
+        self.assertRaises(ValueError, updown.count, "not_a_bracket")
         self.assertRaises(ValueError, updown.count, 1)
         self.assertRaises(ValueError, updown.count, "!!!")
-        self.assertRaises(ValueError, updown.count, "(!)")
-        self.assertRaises(ValueError, updown.count, "(000)")
+        self.assertRaises(ValueError, updown.count, "<!>")
+        self.assertRaises(ValueError, updown.count, "<000>")
 
     def should_return_0_for_blank_string(self):
         self.assertEqual(0, updown.count(""))
 
-    def should_return_1_for_single_up_paren(self):
-        self.assertEqual(1, updown.count('('))
+    def should_return_1_for_single_up_bracket(self):
+        self.assertEqual(1, updown.count('>'))
 
     def should_return_0_for_up_then_down(self):
-        self.assertEqual(0, updown.count('()'))
+        self.assertEqual(0, updown.count('<>'))
 
     def should_return_negative_results(self):
-        self.assertEqual(-1, updown.count(')'))
+        self.assertEqual(-1, updown.count('<'))
 
     def should_return_1_for_up_down_then_up(self):
-        self.assertEqual(1, updown.count('()('))
+        self.assertEqual(1, updown.count('<>>'))
 
     def should_return_3_for_up_up_up(self):
-        self.assertEqual(1, updown.count('((('))
+        self.assertEqual(3, updown.count('>>>'))
